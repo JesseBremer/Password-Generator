c677997e9eec13dfed6a18eebb9d8bb8f3e59878
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-28 17:12:29 -0400

    Password generator takes input from user, then randomizes password string and returns strong password to user!

--- Password Generator Project/task.py
+++ Password Generator Project/task.py
@@ -1,3 +1,6 @@
+import random as random
+from email.contentmanager import raw_data_manager
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -6,5 +9,26 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+for i in range(nr_letters):
+    password += str(random.choice(letters))
+for i in range(nr_numbers):
+    password+= str(random.choice(numbers))
+for i in range(nr_symbols):
+    password+= str(random.choice(symbols))
+
+def randomize_password(pw):
+    chop_password = list(pw)
+    random.shuffle(chop_password)
+    return ''.join(chop_password)
+
+password = randomize_password(password)
+
+print(password)
+
+
+
+
 
 
