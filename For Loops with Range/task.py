f89dccb90416e8c553720375ba95bec7291b436f
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-28 16:41:28 -0400

    FizzBuzz Challenge

--- For Loops with Range/task.py
+++ For Loops with Range/task.py
@@ -1,6 +1,16 @@
-gauss = 0
+# gauss = 0
+#
+# for number in range(1, 101):
+#     gauss += number
+#
+# print(gauss)
 
 for number in range(1, 101):
-    gauss += number
-
-print(gauss)
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
